3762d317c7313e8ce9e80da4c930d2d73361b275
Ryan Ortiz <ryan.r.ortiz@uconn.edu>
2025-08-29 12:53:02 +0000

    I tried

---
+++ lab0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    printf("Hello World\n");
+    int sum = 0;
+    int num = 200;
+    while(num >0)
+    {
+        num--;
+        num--;
+        sum +=num;
+    };
+    printf("%d\n", sum);
+
+}
